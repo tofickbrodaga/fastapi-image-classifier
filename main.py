42aab1d42313de547a03a7f53293d81d3701d059
tofickbrodaga <mshdtlv@gmail.com>
2024-06-07 14:55:37 +0300

    Added app, but not checked

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import JSONResponse
-from predict import predict_label
 import os
-import io
-from PIL import Image
+
+import logging
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+
+
+from predict import predict_label
 
 app = FastAPI()
 
-# Загрузка изображения и получение предсказания
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 @app.post("/predict/")
-async def predict_dish(file: UploadFile = File(...)):
-    if not file.content_type.startswith('image/'):
-        raise HTTPException(status_code=400, detail="Файл должен быть изображением")
+async def predict_dish(request):
+    content_type = request.headers.get('Content-Type')
+    if content_type != 'application/octet-stream':
+        raise HTTPException(status_code=400, detail="Content-Type 'application/octet-stream'")
 
-    image_bytes = await file.read()
-
-    # Создание временного файла для сохранения изображения
+    image_bytes = await request.body()
     temp_image_path = "temp_image.jpg"
     with open(temp_image_path, "wb") as f:
         f.write(image_bytes)
 
-    try:
-        prediction = predict_label(temp_image_path)
-    finally:
-        # Удаление временного файла
-        os.remove(temp_image_path)
+    prediction = predict_label(temp_image_path)
+    os.remove(temp_image_path)
 
     return JSONResponse(content={"prediction": prediction})
 
-
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8099)
--- predict.py
+++ predict.py
@@ -1,12 +1,14 @@
-import torch
-from PIL import Image
-from torchvision import transforms
-import torch.nn as nn
-from torchvision.models import resnet101
+import contextlib
 import os
-import pandas as pd
-from torch.utils.data import Dataset
+import sys
 
+import pandas as pd
+import torch
+import torch.nn as nn
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+from torchvision.models import resnet101
 
 images_dir = 'archive_food/images'
 
@@ -14,11 +16,13 @@ images_dir = 'archive_food/images'
 dataframe = []
 for root, _, files in os.walk(images_dir):
     for file in files:
-        if file.lower().endswith(('png', 'jpg', 'jpeg', 'bmp', 'gif')):
+        if file.lower().endswith(('png', 'jpg', 'jpeg')):
             file_path = os.path.join(root, file)
             label = os.path.basename(root)
             formatted_path = os.path.join(label, file)
             dataframe.append({'path': formatted_path, 'label': label})
+
+dataframe = pd.DataFrame(dataframe)
 
 
 class Food101(Dataset):
@@ -42,11 +46,10 @@ class Food101(Dataset):
 
 
 transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor()
-    ])
+    transforms.Resize((224, 224)),
+    transforms.ToTensor()
+])
 
-dataframe = pd.DataFrame(dataframe)
 food_dataset = Food101(dataframe, images_dir, transform=transform)
 num_classes = len(food_dataset.dataframe['label'].unique())
 
@@ -54,7 +57,8 @@ num_classes = len(food_dataset.dataframe['label'].unique())
 class ResNetClassifier(nn.Module):
     def __init__(self, num_classes):
         super(ResNetClassifier, self).__init__()
-        self.resnet101 = resnet101(pretrained=True)
+        with suppress_stdout():
+            self.resnet101 = resnet101(pretrained=True)
         for param in self.resnet101.parameters():
             param.requires_grad = False
         self.resnet101.fc = nn.Linear(self.resnet101.fc.in_features, num_classes)
@@ -62,8 +66,6 @@ class ResNetClassifier(nn.Module):
     def forward(self, x):
         x = self.resnet101(x)
         return x
-
-model = ResNetClassifier(num_classes=num_classes)
 
 
 class LabelEncoder:
@@ -78,17 +80,33 @@ class LabelEncoder:
     def get_idx(self, label):
         return self.labels.get(label)
 
-# Загрузка обученной модели
-state_dict = torch.load('tested_model-2.pth', map_location=torch.device('cpu'))
+
+@contextlib.contextmanager
+def suppress_stdout():
+    with open(os.devnull, 'w') as devnull:
+        old_stdout = sys.stdout
+        old_stderr = sys.stderr
+        sys.stdout = devnull
+        sys.stderr = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+            sys.stderr = old_stderr
+
+
+model = ResNetClassifier(num_classes=num_classes)
+
+
+state_dict = torch.load('accuracy52.pth', map_location=torch.device('cpu'))
 model.load_state_dict(state_dict)
 
 classes = food_dataset.dataframe['label'].unique()
 label_encoder = LabelEncoder(classes)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Функция классификации
+
 def classify_image(image_path, model, label_encoder, device):
-    # Загрузка и предобработка изображения
     image = Image.open(image_path)
     preprocess = transforms.Compose([
         transforms.Resize((224, 224)),
@@ -96,20 +114,16 @@ def classify_image(image_path, model, label_encoder, device):
     ])
     image_tensor = preprocess(image).unsqueeze(0).to(device)
 
-    # Предсказание
     with torch.no_grad():
         model.eval()
         output = model(image_tensor)
 
-    # Получение индекса предсказанного класса
     _, predicted_idx = torch.max(output, 1)
     predicted_idx = predicted_idx.item()
-
-    # Преобразование индекса в метку класса
     predicted_label = label_encoder.get_label(predicted_idx)
 
     return predicted_label
 
-# Функция предсказания метки
+
 def predict_label(image_path: str):
     return classify_image(image_path, model, label_encoder, device)
